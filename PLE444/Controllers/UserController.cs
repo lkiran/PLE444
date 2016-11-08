858c8b5c6be5e344e8326b0875f49a89ba5dbeba
lkiran <leventkiran@hotmail.com.tr>
2016-11-09 00:03:54 +0300

    Profile View and Assignment Model
    Layout
    + Static content div added
    + Profile page link added
    
    View
    + Profile page added
    
    Models
    + Assingment added
    + UserAssignment added

---
+++ PLE444/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PLE444.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PLE444/Models/Assignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PLE444.Models
+{
+    public class Assignment
+    {
+        public Assignment()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Required]
+        public Guid Id { get; private set; }
+        public Course Course { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+}
