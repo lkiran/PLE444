7f0bdc9c71c4f1aa93e99bb47062fff447e7c085
lkiran <leventkiran@hotmail.com.tr>
2016-11-14 23:43:50 +0300

    Example Drag&Drop Uploader Implemented
    UploaderTest controller is created and in the Index View uploader is
    implemented, which saves files to Upload folder under PLE444 folder.

---
+++ PLE444/Controllers/UploaderTestController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PLE444.Controllers
+{
+	public class UploaderTestController : Controller
+	{
+		// GET: UploaderTest
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost] //must be post method
+        public ActionResult Index(IEnumerable<HttpPostedFileBase> files)
+        {
+            if (files == null)
+                return RedirectToAction("Index", "Home");
+
+			var path = "";  //for path to save
+
+			foreach (var item in files)  //iterate in each file
+			{
+				if (item != null)   //check file is null or not
+				{
+					if (item.ContentLength > 0) //check length of bytes are greater then zero or not
+					{
+						//for checking uploaded file is image or not
+						if (Path.GetExtension(item.FileName).ToLower() == ".jpg"
+							|| Path.GetExtension(item.FileName).ToLower() == ".png"
+						    || Path.GetExtension(item.FileName).ToLower() == ".gif"
+							|| Path.GetExtension(item.FileName).ToLower() == ".jpeg")
+						{
+							path = Path.Combine(Server.MapPath("~/Uploads"), item.FileName);
+							item.SaveAs(path);
+							ViewBag.UploadSuccess = true;
+						}
+					}
+				}
+			}
+			return View();
+		}
+
+	}
+}
