fe1e792a1a3dcd90b9d61c956d05998d4297f2dc
Levent Kıran <leventkiran@hotmail.com.tr>
2019-05-02 21:02:47 +0300

    Project files are fixed

--- PLE.Website/PLE444/ViewModels/CourseViewModel.cs
+++ PLE.Website/PLE444/ViewModels/CourseViewModel.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using PLE.Contract.DTOs;
 using PLE444.Models;
 
 namespace PLE444.ViewModels
